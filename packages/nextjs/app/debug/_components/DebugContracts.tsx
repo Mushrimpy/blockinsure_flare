d35fb56717048cfcf21bbcb92002d0a862962067
Simon Socolow <simonsocolow78@gmail.com>
2025-02-10 15:43:40 +0000

    fixing types so can build

--- packages/nextjs/app/debug/_components/DebugContracts.tsx
+++ packages/nextjs/app/debug/_components/DebugContracts.tsx
@@ -45,11 +45,11 @@ export function DebugContracts() {
                     ? "bg-base-300 hover:bg-base-300 no-animation"
                     : "bg-base-100 hover:bg-secondary"
                     }`}
-                  key={contractName}
+                  key={String(contractName)}
                   onClick={() => setSelectedContract(contractName)}
                 >
-                  {contractName}
-                  {(contractsData[contractName] as GenericContract)?.external && (
+                  {String(contractName)}
+                  {(contractsData[contractName as keyof typeof contractsData] as GenericContract)?.external && (
                     <span className="tooltip tooltip-top tooltip-accent" data-tip="External contract">
                       <BarsArrowUpIcon className="h-4 w-4 cursor-pointer" />
                     </span>
@@ -60,7 +60,7 @@ export function DebugContracts() {
           )}
           {contractNames.map(contractName => (
             <ContractUI
-              key={contractName}
+              key={String(contractName)}
               contractName={contractName}
               className={contractName === selectedContract ? "" : "hidden"}
             />
--- packages/nextjs/hooks/scaffold-eth/useTransactor.tsx
+++ packages/nextjs/hooks/scaffold-eth/useTransactor.tsx
@@ -73,6 +73,11 @@ export const useTransactor = (_walletClient?: WalletClient): TransactionFunc => 
         <TxnNotification message="Waiting for transaction to complete." blockExplorerLink={blockExplorerTxURL} />,
       );
 
+      if (!publicClient) {
+        console.error("Public client is not ready");
+        return;
+      }
+
       transactionReceipt = await publicClient.waitForTransactionReceipt({
         hash: transactionHash,
         confirmations: options?.blockConfirmations,
